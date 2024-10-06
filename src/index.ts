80792df6a1562a80ac945ff1694ed287d7aed2f0
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-10-06 20:47:18 +0900

    Cloudflare Workers: Initial Setting):

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,6 @@
 // TypeScript는 home.html이 무엇인지 모르기 때문에 에러를 발생시킴
 // @ts-ignore
 import home from './home.html';
-import { makeBadge } from './utils';
 
 function handleHome() {
 	// Response로 home만 보내면 text로 인식함. headers를 함께 보내서 브라우저가 실행할 수 있도록 함.
@@ -28,35 +27,6 @@ function handleHome() {
 function handleNotFound() {
 	return new Response(null, {
 		status: 404,
-	});
-}
-
-function handleBadRequest() {
-	return new Response(null, {
-		status: 400,
-	});
-}
-
-async function handleVisit(searchParams: URLSearchParams, env: Env) {
-	console.log(searchParams.toString());
-	// search parameters에서 page parameter를 받아옴
-	const page = searchParams.get('page');
-	console.log(page);
-	if (!page) {
-		return handleBadRequest();
-	}
-	const kvPage = await env.DB.get(page);
-	let value = 1;
-	if (!kvPage) {
-		await env.DB.put(page, value + '');
-	} else {
-		value = parseInt(kvPage) + 1;
-		await env.DB.put(page, value + '');
-	}
-	return new Response(makeBadge(value), {
-		headers: {
-			'Content-Type': 'image/svg+xml;chartset=utf-8',
-		},
 	});
 }
 
